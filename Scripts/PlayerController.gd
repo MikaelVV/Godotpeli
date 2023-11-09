3e17ff517c61d8a0d163d19d56523bdaa7aefe78
Mikael <50752113+MikaelVV@users.noreply.github.com>
2023-11-10 00:32:43 +0200

    Added mouse camera movement to player

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -4,9 +4,25 @@ extends CharacterBody3D
 @export var SPEED = 5
 @export var JUMP_VELOCITY = 4
 
+# Määritellään pelaajan pivot ja camera variablet.
+@onready var pivot := $Pivot
+@onready var camera := $Pivot/Camera3D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+# Function "kuuntelee" kaikkia inputteja, joka tässä tapauksessa on hiiri
+# ja määrittelee hiiren liikkuvuuden kameran kanssa.
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"): # ui_cancel tarkoittaa Esc näppäintä.
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion: # Alla määrittellään hiiren herkkyys.
+			pivot.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60)) # Tässä limitoidaan pelaajan kameran rotaatiota, että pelaaja ei voi katsoa 360 astetta ympäri.
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,7 +36,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
