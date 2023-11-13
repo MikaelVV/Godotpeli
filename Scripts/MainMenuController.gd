2a6e757c66451802743fc505b76b18d1f3db4940
Mikael <50752113+MikaelVV@users.noreply.github.com>
2023-11-13 14:52:13 +0200

    Made new Main01 scene

--- Scripts/MainMenuController.gd
+++ Scripts/MainMenuController.gd
@@ -11,7 +11,7 @@ func _process(_delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/node_3d.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main01.tscn")
 
 
 func _on_quit_button_pressed():
